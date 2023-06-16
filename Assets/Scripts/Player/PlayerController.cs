8f0bfbafc4b4d36aca6ef3d06725d14a6b7c1a07
FabioBetancourt <fabiobetancourt7@gmail.com>
2023-06-16 12:34:48 -0500

    fixed move player with acceleration and desaceleration

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,7 +8,10 @@ namespace Player
         public PlayerInput playerInput;
         private Vector2 _movementInput;
         private Rigidbody rb;
-        public float speed = 5.0f; // Velocidad de movimiento
+        public float speed = 5.0f;
+        public bool isOnGround = true;
+        public float accelaration = 0.1f;
+        public float decelaration = 0.1f;
 
         private void Awake()
         {
@@ -23,20 +26,36 @@ namespace Player
 
         private void FixedUpdate()
         {
-            Vector3 movement = new Vector3(_movementInput.x, 0, _movementInput.y) * (speed * Time.fixedDeltaTime);
-            rb.MovePosition(rb.position + movement);
+            Vector3 movement = new Vector3(_movementInput.x, 0, _movementInput.y) * (speed);
+            if (movement.magnitude > 0.1f)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3( movement.x, rb.velocity.y, movement.z), accelaration);
+            }else
+            {
+                rb.velocity = Vector3.Lerp( rb.velocity, new Vector3( movement.x, rb.velocity.y, movement.z), decelaration);
+            }
 
-            if (playerInput.actions["Jump"].triggered && IsGrounded())
+            //new Vector3(movement.x, rb.velocity.y, movement.z);
+            
+            if (playerInput.actions["Jump"].triggered && isOnGround)
             {
                 rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+                isOnGround = false;
             }
         }
 
+        /*
         private bool IsGrounded()
         {
             float distance = GetComponent<Collider>().bounds.extents.y + 0.1f;
             Ray ray = new Ray(transform.position, Vector3.down);
             return Physics.Raycast(ray, distance);
+        }
+        */
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            isOnGround = true;
         }
     }
 }
