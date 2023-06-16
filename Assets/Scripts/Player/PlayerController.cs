3119fefd1af90708127dc4eb019d8a7fa4316924
FabioBetancourt <fabiobetancourt7@gmail.com>
2023-06-15 23:52:31 -0500

    added map input systen and script about the map

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        public PlayerInput playerInput;
+        private Vector2 _movementInput;
+        private Rigidbody rb;
+        public float speed = 5.0f; // Velocidad de movimiento
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _movementInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 movement = new Vector3(_movementInput.x, 0, _movementInput.y) * (speed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + movement);
+
+            if (playerInput.actions["Jump"].triggered && IsGrounded())
+            {
+                rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            float distance = GetComponent<Collider>().bounds.extents.y + 0.1f;
+            Ray ray = new Ray(transform.position, Vector3.down);
+            return Physics.Raycast(ray, distance);
+        }
     }
 }
