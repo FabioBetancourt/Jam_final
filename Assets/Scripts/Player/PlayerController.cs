6111dbb5a91e172311bb451fad15d8957a001fa0
FabioBetancourt <fabiobetancourt7@gmail.com>
2023-06-18 09:21:09 -0500

    dont worry about this

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [Header("Dont touch this")]
+        [Tooltip("Really dont touch this")]
         public PlayerInput playerInput;
+        [Header("Speed")]
+        [Tooltip("This is the speed of the player")]
+        public float speed = 5.0f;
+        [Header("Acceleration")]
+        [Tooltip("This is the acceleration of the player, the higher the number the faster the player will reach max speed")]
+        public float accelaration = 0.1f;
+        [FormerlySerializedAs("decelaration")]
+        [Header("Deceleration")]
+        [Tooltip("This is the deceleration of the player, the higher the number the faster the player will stop")]
+        public float deceleration = 0.1f;
         private Vector2 _movementInput;
         private Rigidbody rb;
-        public float speed = 5.0f;
-        public bool isOnGround = true;
-        public float accelaration = 0.1f;
-        public float decelaration = 0.1f;
+        public Basket basket;
+
 
         private void Awake()
         {
@@ -22,6 +33,7 @@ namespace Player
         private void Update()
         {
             _movementInput = playerInput.actions["Move"].ReadValue<Vector2>();
+            
         }
 
         private void FixedUpdate()
@@ -32,30 +44,23 @@ namespace Player
                 rb.velocity = Vector3.Lerp(rb.velocity, new Vector3( movement.x, rb.velocity.y, movement.z), accelaration);
             }else
             {
-                rb.velocity = Vector3.Lerp( rb.velocity, new Vector3( movement.x, rb.velocity.y, movement.z), decelaration);
+                rb.velocity = Vector3.Lerp( rb.velocity, new Vector3( movement.x, rb.velocity.y, movement.z), deceleration);
             }
-
-            //new Vector3(movement.x, rb.velocity.y, movement.z);
             
-            if (playerInput.actions["Jump"].triggered && isOnGround)
+            if (playerInput.actions["Jump"].triggered && IsGrounded())
             {
                 rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-                isOnGround = false;
+                
             }
         }
 
-        /*
+
         private bool IsGrounded()
         {
             float distance = GetComponent<Collider>().bounds.extents.y + 0.1f;
             Ray ray = new Ray(transform.position, Vector3.down);
             return Physics.Raycast(ray, distance);
         }
-        */
         
-        private void OnCollisionEnter(Collision other)
-        {
-            isOnGround = true;
-        }
     }
 }
